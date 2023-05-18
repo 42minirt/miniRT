f9d6255b1cd0b10d3a7c203c3be0a0a6de21bc56
takira <ca.fie.0220@gmail.com>
2023-05-19 08:11:30 +0900

    add parsing camera

--- srcs/constructor/get_ambient_setting.c
+++ srcs/constructor/get_ambient_setting.c
@@ -17,7 +17,7 @@ static bool	is_ambient_already_exists(t_scene_info *scene)
 	return (scene->brightness >= 0);
 }
 
-// A   lightning_ratio[0,1]   RGB[0,255]
+// #Ambient  lightning_ratio[0,1]  RGB[0,255]
 t_parse_res	get_ambient_setting(const char *line, t_scene_info *scene)
 {
 	size_t	idx;
--- srcs/constructor/get_camera_setting.c
+++ srcs/constructor/get_camera_setting.c
@@ -12,11 +12,26 @@
 
 #include "minirt.h"
 
-// #Camera     viewpoint(xyz)         normalized_orientation_vec[-1,1]   FOV[0,180]
+static bool	is_camera_already_exists(t_camera_info *camera)
+{
+	return (camera->fov_deg >= 0);
+}
+
+// #Camera  viewpoint(xyz)  normalized_orientation_vec[-1,1]  FOV[0,180]
 t_parse_res	get_camera_setting(const char *line, t_camera_info *camera)
 {
-	(void)line;
-	(void)camera;
-	// todo: already exists
+	size_t	idx;
+
+	if (is_camera_already_exists(camera))
+		return (ERROR_MULTIPLE_ID);
+	idx = 0;
+	if (parse_vec(line, &camera->position, &idx) == FAILURE)
+		return (ERROR_INVALID_ARG);
+	if (parse_vec(line, &camera->direction, &idx) == FAILURE)
+		return (ERROR_INVALID_ARG);
+	if (parse_double(line, &camera->fov_deg, &idx) == FAILURE)
+		return (ERROR_INVALID_ARG);
+	if (line[idx])
+		return (ERROR_TOO_MANY_INFO);
 	return (PASS);
 }
