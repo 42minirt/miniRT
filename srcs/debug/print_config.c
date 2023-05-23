60070c88f988786837e6fa3bc57638814ca64efa
ak0327 <ca.fie.0220@gmail.com>
2023-05-23 12:27:09 +0900

    fix: debug print alignment

--- srcs/debug/print_config.c
+++ srcs/debug/print_config.c
@@ -33,7 +33,7 @@ static void	print_lights(t_scene_info *scene)
 		else
 			printf("  Spot Light  : (%5.1f, %5.1f, %5.1f), " \
 			"                                   %5.1f,   %5.1f," \
-			"           (%5.1f, %5.1f, %5.1f)\n", \
+			"  (%5.1f, %5.1f, %5.1f)\n", \
 			v.x, v.y, v.z, light->brightness, light->sl_angle, c.r, c.g, c.b);
 		node = node->next;
 	}
