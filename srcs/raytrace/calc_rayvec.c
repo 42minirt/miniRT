417f3c1f703c91553473d064d757736e4c6ea557
molhot <sakata19991214@gmail.com>
2023-05-26 11:46:03 +0900

    視点からスクリーンの距離を求める関数を追加

--- srcs/raytrace/calc_rayvec.c
+++ srcs/raytrace/calc_rayvec.c
@@ -6,13 +6,18 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 00:42:09 by user              #+#    #+#             */
-/*   Updated: 2023/05/26 01:31:00 by user             ###   ########.fr       */
+/*   Updated: 2023/05/26 11:42:18 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-double	caldc_eye2scrcen_distance(t_camera_info *s_camera_info)
+double	calc_eye2scrcen_distance(t_camera_info *camera_info)
+{
+	return (WINDOW_WIDTH / (2.0 * camera_info->fov_deg));
+}
+
+void	ready_scr2world_ex(t_vec *scr2world_ex, t_camera_info *camera_info)
 {
 	
 }
@@ -26,11 +31,11 @@ t_ray	red_rayvec(t_camera_info *camera_info, double x, double y)
 	t_vec	t_rey_n;//ここはt_camera_infoの中の変数でいい気もする
 
 	//視点からスクリーンまでの距離を求める
-
+	//calc_eye2scrcen_distance
 	//上記からeye2scr(の中心)のベクトルを求める
-
+	eye2scr = k_vec(calc_eye2scrcen_distance(camera_info), camera_info->direction);
 	//カメラの方向ベクトルを用いてスクリーンのx方向のベクトルを表現する
-
+	ready_scr2world_ex(&scr2world_ex, camera_info);
 	//スクリーンのy方向のベクトルをx方向のベクトルとカメラの方向ベクトルの外積で求める
 
 	//t_rayというベクトルを求める（カメラからスクリーン状のある点までのベクトル）
