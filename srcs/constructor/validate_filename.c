21d6753d278b62af076718136158789cb3b85820
takira <ca.fie.0220@gmail.com>
2023-06-10 11:48:02 +0900

    update(constructor): add validate input file 'is_directory'

--- srcs/constructor/validate_filename.c
+++ srcs/constructor/validate_filename.c
@@ -35,6 +35,36 @@ static int	try_open(const char *path)
 	return (SUCCESS);
 }
 
+static bool	validate_extension(const char *input, const char *correct)
+{
+	size_t	idx;
+
+	if (!input || !correct)
+		return (false);
+	idx = 0;
+	while (input[idx] && (ft_tolower(input[idx]) == correct[idx]))
+		idx++;
+	return (input[idx] == correct[idx]);
+}
+
+static bool	is_dir(const char *path)
+{
+	const size_t	path_len = ft_strlen_ns(path);
+	char			*test_path;
+	int				try_open_res;
+
+	if (path_len == 0)
+		return (false);
+	if (path[path_len - 1] == '/')
+		return (true);
+	test_path = (char *)ft_calloc(path_len + 2, sizeof(char));
+	ft_strlcat(test_path, path, path_len + 1);
+	test_path[path_len] = '/';
+	try_open_res = try_open(test_path);
+	free(test_path);
+	return (try_open_res == SUCCESS);
+}
+
 int	validate_filename(const char *path, const char *extension)
 {
 	const char		*filename = get_filename(path);
@@ -42,11 +72,13 @@ int	validate_filename(const char *path, const char *extension)
 	const size_t	extension_len = ft_strlen_ns(extension);
 	int				try_open_res;
 
+	if (is_dir(path))
+		return (FAILURE);
 	if (cnt_chr_in_str('.', filename) > 1)
 		return (FAILURE);
 	if (filename_len <= extension_len)
 		return (FAILURE);
-	if (!is_equal_strings(&filename[filename_len - extension_len], extension))
+	if (!validate_extension(&filename[filename_len - extension_len], extension))
 		return (FAILURE);
 	try_open_res = try_open(path);
 	return (try_open_res);
